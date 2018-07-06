da531f055b81187ca43029ea60ff0565b2760993
lakshmi-sravani <lakshmisravani.kolla@walkingtree.tech>
2018-07-06 16:36:44 +0530

    first commit

--- src/app/shop.service.ts
+++ src/app/shop.service.ts
@@ -4,22 +4,33 @@ import { Ingredient } from './ingredient.model';
   providedIn: 'root'
 })
 export class ShopService {
-name1:string='';
-name2:number;
-items:Ingredient[]=[
-  new Ingredient("apple",5),
-  new Ingredient("tomato",10)
-];
-add(name1,name2){
- let x= new Ingredient(name1,name2);
-this.items.push(x);
-this.name1='';
-this.name2;
-return this.items;
-}
- get(){
-  this.items.splice(0,2)
+  name1:string='';
+  name2:number;
+  // z:number;
+  items:Ingredient[]=[
+    new Ingredient("apple",5),
+    new Ingredient("tomato",10)
+  ];
+  add(name1,name2,z){
+ if(z==undefined){
+let x= new Ingredient(name1,name2);
+    this.items.push(x);
+    this.name1='';
+    this.name2;
+    
+    return this.items;
+ }
+  else{
+
+   let x= new Ingredient(name1,name2);
+
+   this.items[z]=x;
+
+    return this.items;
+  }  
   }
+
+  
   constructor() { }
 }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -12,17 +12,33 @@ export class ShoppingListComponent implements OnInit {
 name1:string;
 name2:number;
 temp:{};
-items=[{}];
+items:Ingredient[];
+ z:number;
   add(){
-    debugger
+  //  debugger;
   console.log(this.name1);
+  console.log(this.z);
   // let s = this.name1;
-  this.items=this.shop.add(this.name1,this.name2);
+ 
+  this.items=this.shop.add(this.name1,this.name2,this.z);
+
+
   console.log(this.items);
 
 }
- get(){
-  this.shop.get();
+listen(item,i){
+  
+  console.log(item);
+  console.log(i);
+  this.name1=item.name1;
+  this.name2=item.name2;
+  this.z=i;
+  // this.items.splice(ind,1);
+}
+ delete(){
+
+   this.items.splice(this.z,1);
+  
   }
  
   constructor(private shop:ShopService) { }
